9e77993110dfdc982761fae841a7552b960e1bc9
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-12-03 23:34:07 +0100

    Update nginx-script.sh

--- srcs/requirements/nginx/tools/nginx-script.sh
+++ srcs/requirements/nginx/tools/nginx-script.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+# # Ensure ${DOMAIN_NAME} is not empty.
+# if [ -z "${DOMAIN_NAME}" ]; then
+#     echo "DOMAIN_NAME is not set. Exiting."
+#     exit 1
+# fi
+
+# # Start NGINX in the foreground
+# echo "Starting nginx..."
+# nginx -g "daemon off;"
+
+
+
+# NOTE:
+# Path is wrong : "/etc/ssl/private/nginx-selfsigned.key" - "/etc/ssl/certs/nginx-selfsigned.crt"
+# should be : /etc/nginx/ssl/ -> both of them
+
+
 # Ensure ${DOMAIN_NAME} is not empty.
 if [ -z "${DOMAIN_NAME}" ]; then
     echo "DOMAIN_NAME is not set. Exiting."
     exit 1
 fi
+
+# # Create necessary directories and set permissions as root
+# mkdir -p /etc/nginx/ssl/
+# chmod +x /etc/nginx/ssl/
 
 # Create a self-signed SSL certificate for TLS (or use your own cert files)
 echo "Creating a self-signed SSL certificate..."
@@ -13,12 +34,12 @@ openssl req \
     -nodes \
     -days 365 \
     -newkey rsa:2048 \
-    -keyout "/etc/ssl/private/nginx-selfsigned.key" \
+    -keyout "/etc/nginx/ssl/nginx-selfsigned.key" \
     -subj "/C=NL/ST=Holland/L=Amsterdam/O=Codam/CN=${DOMAIN_NAME}" \
-    -out "/etc/ssl/certs/nginx-selfsigned.crt"
+    -out "/etc/nginx/ssl/nginx-selfsigned.crt"
 
 # Check if the certificate files were created successfully
-if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ] || [ ! -f /etc/ssl/private/nginx-selfsigned.key ]; then
+if [ ! -f /etc/nginx/ssl/nginx-selfsigned.crt ] || [ ! -f /etc/nginx/ssl/nginx-selfsigned.key ]; then
     echo "Error: SSL certificate creation failed. Exiting."
     exit 1
 else
