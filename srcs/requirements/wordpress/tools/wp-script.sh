ebd041bbbfbc2974a6ccc6e228a433a50f84a882
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2025-02-07 15:24:14 +0100

    Update wp-script.sh

--- srcs/requirements/wordpress/tools/wp-script.sh
+++ srcs/requirements/wordpress/tools/wp-script.sh
@@ -9,58 +9,58 @@ chown -R 755 /var/www/*;
 # Download/Install WordPress if not already present
 if [ ! -f wp-config.php ]; then
 
-    # Install WP-CLI (wp command)
+    	# Install WP-CLI (wp command)
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
     
-    cd /var/www/html/wordpress
+    	cd /var/www/html/wordpress
     
-    # Downloading WordPress"
-    wp core download \
+    	# Downloading WordPress"
+   	wp core download \
         --path="/var/www/html/wordpress/" \
         --allow-root
-    echo "WordPress downloaded."
+    	echo "WordPress downloaded."
 
-    # Wait for MariaDB to be ready
-    echo "Waiting for MariaDB to connect..."
-    until mysqladmin -h${WP_DATABASE_HOST} -u${DB_USER} -p${DB_USER_PASSWORD} ping; do
-           sleep 2
-    done
+   	# Wait for MariaDB to be ready
+    	echo "Waiting for MariaDB to connect..."
+    	until mysqladmin -h${WP_DATABASE_HOST} -u${DB_USER} -p${DB_USER_PASSWORD} ping; do
+        	sleep 2
+    	done
 
-    # Create wp-config.php, because I didn't create 'wp-config.php' manully
-    # NOTE: These variables are passed after Runtime of the container
-    echo "Creating WordPress Configuration..."
+    	# Create wp-config.php, because I didn't create 'wp-config.php' manully
+    	# NOTE: These variables are passed after Runtime of the container
+    	echo "Creating WordPress Configuration..."
 	wp config create \
-        --path="/var/www/html/wordpress/" \
+        	--path="/var/www/html/wordpress/" \
 		--dbname="${WP_DATABASE_NAME}" \
 		--dbuser="${DB_USER}" \
 		--dbpass="${DB_USER_PASSWORD}" \
 		--dbhost="${WP_DATABASE_HOST}" \
 		--allow-root
 
-    # Create Wordpress Admin
+    	# Create Wordpress Admin
 	echo "Creating Wordpress Admin..."
-    wp core install \
-        --path="/var/www/html/wordpress/" \
-        --url="${DOMAIN_NAME}" \
-        --title="inception" \
-        --admin_user="${WP_ADMIN}" \
-        --admin_password="${WP_ADMIN_PASSWORD}" \
-        --admin_email="${WP_ADMIN_EMAIL}" \
-        --allow-root
+    	wp core install \
+        	--path="/var/www/html/wordpress/" \
+        	--url="${DOMAIN_NAME}" \
+        	--title="inception" \
+        	--admin_user="${WP_ADMIN}" \
+        	--admin_password="${WP_ADMIN_PASSWORD}" \
+        	--admin_email="${WP_ADMIN_EMAIL}" \
+        	--allow-root
 
-    # Create Wordpress User
-    echo "Creating Wordpress User..." 
+   	# Create Wordpress User
+    	echo "Creating Wordpress User..." 
 	wp user create ${WP_USER} ${WP_USER_EMAIL} \
-        --path="/var/www/html/wordpress/" \
-        --user_pass="${WP_USER_PASSWORD}" \
-	--role=editor # or --role=author
-        --allow-root
+        	--path="/var/www/html/wordpress/" \
+        	--user_pass="${WP_USER_PASSWORD}" \
+		--role=editor # or --role=author
+        	--allow-root
 
-    echo "WordPress setup complete."
+    	echo "WordPress setup complete."
 else
-    echo "WordPress is already downloaded and setup."
+    	echo "WordPress is already downloaded and setup."
 fi
 
 # Start PHP-FPM in the foreground
